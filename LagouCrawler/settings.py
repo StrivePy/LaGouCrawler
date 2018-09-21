c86e44c9257d946b05af3c49b7dc3a9fe1414a64
StrivePy <1013974267@qq.com>
2018-09-21 16:54:32 +0800

    Request With AbuYun

--- LagouCrawler/settings.py
+++ LagouCrawler/settings.py
@@ -103,9 +103,9 @@ ITEM_PIPELINES = {
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 # 用户名
-USERNAME = '17386601610'
+USERNAME = '17386601XXX'
 # 用户密码
-PASSWORD = '175458778sd'
+PASSWORD = '17545877XXX'
 
 # 选择城市
 CITY = '杭州站'
